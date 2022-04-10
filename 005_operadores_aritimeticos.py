f47851bd99b3a15bea18714be34b6e8097f5caf5
Danilo Araujo Mota <88139297+DaniloArujo@users.noreply.github.com>
2022-04-10 11:21:57 -0300

    Add files via upload

---
+++ 005_operadores_aritimeticos.py
@@ -0,0 +1,17 @@
+# + adição, - subtração, * multiplicação, / divisão,
+# ** potencia, // divisão inteira, % resto da divisão.
+
+
+x = int(input("Digite um numero: "))
+y = int(input("Digite um outro numero: "))
+
+s = x + y
+m = x * y
+d = x / y
+di = x // y
+e = x ** y
+rd = x % y
+
+print(f"A soma é {s}, a multiplicação é {m}, a divisão é {d} "
+      f"a divisão inteira é {di}, a exponenciação é {e} e "
+      f"o resto da divisão é {rd}.")
