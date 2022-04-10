29d09eefee33064824544c1cd1cac479bc60a65b
Danilo Araujo Mota <88139297+DaniloArujo@users.noreply.github.com>
2022-04-10 10:15:51 -0300

    Add files via upload

---
+++ 003_tipos_primitivos.py
@@ -0,0 +1,17 @@
+
+n1 = int(input("Digite um numro: "))
+n2 = float(input("Digite um numero"))
+n3 = str(input("Digite uma palavra: "))
+n4 = bool(input("Digite um boleano"))
+
+print(n1)
+print(type(n1))
+
+print(n2)
+print(type(n2))
+
+print(n3)
+print(type(n3))
+
+print(n4)
+print(type(n4))
---
+++ 004_operacoes_python.py
@@ -0,0 +1,6 @@
+n1 = int(input("Escreva um numero: "))
+n2 = int(input("Escreva um outro numero: "))
+
+n3 = n1 + n2
+
+print(f'A soma entre {n1} e {n2} é: {n3}')
