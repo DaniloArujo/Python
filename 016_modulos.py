fbad2a7abeb699b9983b26bdd91fc84907098ec3
Danilo <daniloaraujomota@gmail.com>
2023-01-16 16:05:00 -0300

    .

---
+++ 016_modulos.py
@@ -0,0 +1,11 @@
+import random
+from math import sqrt, floor
+
+numero = int(input('Digite um numero: '))
+
+raiz = sqrt(numero)
+
+print(f'A raiz de {numero} é igual a {floor(raiz)}')
+
+num = random.randint(0,100)
+print(num)
---
+++ 017_pacotes.py
@@ -0,0 +1,5 @@
+#pip install (nome do pacote)
+
+import emoji
+
+print(emoji.emojize(":smile:", language='alias'))
