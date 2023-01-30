e2a7a9de8f198261c02e642031444aa28d6c1495
Danilo <daniloaraujomota@gmail.com>
2023-01-30 14:56:19 -0300

    .

---
+++ 028_media.py
@@ -0,0 +1,13 @@
+
+n1 = int(input('Digite a primeira nota: '))
+n2 = int(input('Digite a segunda nota: '))
+
+media = (n1 + n2)/2
+
+if media < 5 :
+    print('Reprovado')
+
+else:
+    print('Aprovado')
+
+
---
+++ 029_carrovelho.py
@@ -0,0 +1,2 @@
+tempo = int(input('Quantos anos o carro tem ? '))
+print('Carro velho' if tempo >= 3 else 'Carro novo')
