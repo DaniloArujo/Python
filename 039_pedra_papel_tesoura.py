6a3d7562825c1c5858e9cee55b9e87dbcd4e6f65
Danilo <daniloaraujomota@gmail.com>
2023-02-08 03:28:59 -0300

    .

--- 039_pedra_papel_tesoura.py
+++ 039_pedra_papel_tesoura.py
@@ -31,4 +31,47 @@ while True:
         print('Computador jogou Tesoura')
         print('Você jogou pedra')
         print('\nVocê ganhou! \n')
+        print('###############################################################')
+
+    elif jogada == 2 and comp == 'Pedra' :
+        print('###############################################################')
+        print('Computador jogou Pedra')
+        print('Você jogou papel')
+        print('\nVocê ganhou! \n')
+        print('###############################################################')
+    
+    elif jogada == 2 and comp == 'Papel' :
+        print('###############################################################')
+        print('Computador jogou Papel')
+        print('Você jogou papel')
+        print('\nEmpate! \n')
+        print('###############################################################')
+
+    elif jogada == 2 and comp == 'Tesoura' :
+        print('###############################################################')
+        print('Computador jogou Tesoura')
+        print('Você jogou papel')
+        print('\nVocê perdeu! \n')
+        print('###############################################################') 
+
+    elif jogada == 3 and comp == 'Pedra' :
+        print('###############################################################')
+        print('Computador jogou Pedra')
+        print('Você jogou tesoura')
+        print('\nVocê perdeu! \n')
+        print('###############################################################')
+    
+
+    elif jogada == 3 and comp == 'Papel' :
+        print('###############################################################')
+        print('Computador jogou Papel')
+        print('Você jogou tesoura')
+        print('\nVocê ganhou! \n')
+        print('###############################################################')
+
+    elif jogada == 3 and comp == 'Tesoura' :
+        print('###############################################################')
+        print('Computador jogou Tesoura')
+        print('Você jogou tesoura')
+        print('\nEmpate! \n')
         print('###############################################################')   
