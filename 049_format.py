a9eac4051eff82539bd6fe7b35a055c365566da3
Danilo <daniloaraujomota@gmail.com>
2023-02-19 01:44:28 -0300

    .

--- 049_format.py
+++ 049_format.py
@@ -5,3 +5,7 @@ num = 157.4
 print(f'{nome:20} tem {num:.3f}')
 print(f'{4+5}')
 print(f'{ nome.isalpha() }')
+print(f'{nome:^40}')
+print(f'{nome:-^40}')
+print(f'{nome:->40}')
+print(f'{nome:-<40}')
