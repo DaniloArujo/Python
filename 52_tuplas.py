e12209233a7fc7b55f4ee99dad6615cdbfd8da3e
Danilo <daniloaraujomota@gmail.com>
2023-02-20 11:37:20 -0300

    .

---
+++ 52_tuplas.py
@@ -0,0 +1,20 @@
+
+lanche = ('hamburger','suco','pizza','pudim')
+
+print(lanche)
+
+print(lanche[2])
+
+
+#lembrar de que o ultimo numero é ignorado
+print(lanche[0:2])
+
+print(lanche[1:])
+
+#ultimo elemento (indice ao contrário)
+print(lanche[-1])
+
+
+
+for comida in lanche:
+    print(comida)
