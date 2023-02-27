3e289505b2eb60eecec7fe2fbe686068558b5487
Danilo <daniloaraujomota@gmail.com>
2023-02-27 01:29:08 -0300

    .

--- 065_maior_menor_peso.py
+++ 065_maior_menor_peso.py
@@ -4,20 +4,39 @@
 #C) Uma listagem com as pessoas mais leves.
 
 lista_pessoas = list()
-tamanho_lista = 0
+temp = list()
+maior = menor = 0
+
 
 while True:
-    pessoa = list()
-
-    nome = input('digite o nome da pessoa: ')
-    peso = float(input('Digite o peso da pessoa: '))
     
-    pessoa.append(nome)
-    pessoa.append(peso)
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    if len(lista_pessoas) == 0:
+        menor = maior = temp[1]
+    
+    else:
+        if temp[1] < menor:
+            menor = temp[1]
 
-    lista_pessoas.append(pessoa)
+        if temp[1] > maior:
+            maior = temp[1]
+    lista_pessoas.append(temp[:])
+    temp.clear()
     
     parar = input('Deseja continuar?(n)')
     if parar in "Nn":
         break
+
+
+print('-='*30)
+print(f'voce cadastrou {len(lista_pessoas)} pessoas')
 print(lista_pessoas)
+
+for pessoa in lista_pessoas:
+    if pessoa[1] == maior:
+        print(f'{pessoa} tem um dos menores pesos: {pessoa[1]}')
+
+for pessoa in lista_pessoas:
+    if pessoa[1] == menor:
+        print(f'{pessoa} tem um dos menores pesos {pessoa[1]}')
