59f489e933c76e3f4a26b8cd6fbdf1bb228e7963
Danilo <daniloaraujomota@gmail.com>
2023-02-27 09:09:13 -0300

    .

--- 067_matriz.py
+++ 067_matriz.py
@@ -1,1 +1,10 @@
 #Crie um programa que declare uma matriz de dimensão 3×3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
+
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
+
+for x in range(3):
+    for y in range(3):
+        matriz [x][y] = int(input(f'Digite um valor para a posição [{x}][{y}]: '))
+
+for elem in matriz:
+    print(elem)
