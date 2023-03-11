1813e17d251bb844f136ef0e23c5d6df542528d4
Danilo <daniloaraujomota@gmail.com>
2023-03-11 02:20:00 -0300

    .

--- 071_notas_dicionarios.py
+++ 071_notas_dicionarios.py
@@ -4,4 +4,10 @@ aluno = dict()
 aluno['Nome'] = input('Nome: ')
 aluno['Nota'] = float(input('Nota: '))
 
-print(f'O aluno {aluno["Nome"]} tirou {aluno["Nota"]} na prova.')
+if aluno['Nota'] < 7:
+    aluno['situacao'] = 'reprovado'
+
+else:
+    aluno['situacao'] = 'aprovado'
+   
+print(f'O aluno {aluno["Nome"]} tirou {aluno["Nota"]} na prova e está {aluno["situacao"]}.')
